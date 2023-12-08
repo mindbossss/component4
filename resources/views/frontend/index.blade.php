465f21e348f55d31cbdb39ef75d619cbbb3ae053
mindbossss <ddrdkg@gmail.com>
2023-12-08 19:48:48 +0530

    changes

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,6 +2,35 @@
 @section('main')
        <center><h1>Welcome to Lab Test Hub</h1></center>
 
+       <div class="slide active" style="background-color:#FFF886">
+        <div class="row">
+            <div class="col-2">
+                <h1>Welcoome to health test lab</br></h1>
+                <h2>Elevating Healthcare Through Accurate Testing.</h2>
+                <p class="desc">"Embracing Health Together. Wellness is all the sweeter when shared through accurate diagnostics and compassionate care."</p>
+            </div>
+            <div class="col-2">
+                <img src="Images/page.png" width="500" height="600">
+            </div>
+        </div>
+    </div>
+
+    <div class="special-offer">
+        <div class="container">
+            <h2 class="section-title">Special Offer</h2>
+            <div class="row">
+                <div class="col-2">
+                    <img src="Images/HealthCheckup.png" width="500" height="600" alt="Health Checkup Image">
+                </div>
+                <div class="col-2">
+                    <h3>Take Charge of Your Health Today!</h3>
+                    <p>Enjoy a Special Health Checkup Package to prioritize your well-being.</p>
+                    <h3>Limited Time Offer - Act Now!</h3>
+                    <i>Available until December 31, 2023</i>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 @endsection
